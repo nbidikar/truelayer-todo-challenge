f36f2d4c6c707a6e83b25446f2c49aff69ff5cfa
Niti Bidikar <nitibidikar@gmail.com>
2020-03-23 12:02:45 +0100

    Initial project setup with Webpack

---
+++ src/Root.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./app.global.css";
+
+class Root extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Todo Challenge!</h1>
+      </div>
+    );
+  }
+}
+
+export default Root;
